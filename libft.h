650209bd7bb01de09a58079ea4346fc3863d6cde
Andrei mihai FLOREA <anflorea@e1p6.cluj.42.fr>
2015-10-21 14:59:51 +0200

    ft_strlen (untested)

---
+++ libft.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anflorea <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/10/21 14:46:53 by anflorea          #+#    #+#             */
+/*   Updated: 2015/10/21 14:59:03 by anflorea         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LIBFT_H
+# define FT_LIBFT_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+
+/* TODO - remove this before deployment */
+
+# include <stdio.h>
+
+/* ----------------------------------- */
+
+size_t	ft_strlen(const char *s);
+
+#endif
