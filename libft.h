16608173ce5e0334fd8a37105123b53ed78ae34c
Andrei mihai FLOREA <anflorea@e1p46.cluj.42.fr>
2015-10-28 16:26:06 +0100

    strncpy (untested)

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: anflorea <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/10/21 14:46:53 by anflorea          #+#    #+#             */
-/*   Updated: 2015/10/23 12:34:32 by anflorea         ###   ########.fr       */
+/*   Updated: 2015/10/28 16:22:44 by anflorea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,5 +36,6 @@ char	*ft_strdup(const char *s1);
 int		ft_strcmp(const char *s1, const char *s2);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strcpy(char *dst, const char *src);
+char	*ft_strncpy(char *dst, const char *src, size_t n);
 
 #endif
