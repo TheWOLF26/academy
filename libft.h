98c19ecc293cc570388814014af2b7da4cfbfbd5
Andrei mihai FLOREA <anflorea@e1p46.cluj.42.fr>
2015-10-28 18:13:57 +0100

    strchr (untested)

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: anflorea <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/10/21 14:46:53 by anflorea          #+#    #+#             */
-/*   Updated: 2015/10/28 18:03:38 by anflorea         ###   ########.fr       */
+/*   Updated: 2015/10/28 18:13:36 by anflorea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,5 +38,6 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 char	*ft_strcpy(char *dst, const char *src);
 char	*ft_strncpy(char *dst, const char *src, size_t n);
 char	*ft_strcat(char *s1, const char *s2);
+char	*ft_strchr(const char *s, int c);
 
 #endif
